65905f61b7ca033b0d382b442b411e673fa7de93
Gratitude101 <agabaiduandrew@gmail.com>
2024-08-28 13:11:22 +0100

    first selenium start with py

---
+++ Test1.py
@@ -0,0 +1,15 @@
+import time
+from webbrowser import Chrome
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+driver.get("https://google.com")
+
+time.sleep(2)
+driver.close()
+driver.quit()
+print("Done")
